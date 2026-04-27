0d307efde4fad30c92d2ccbe44361b41d51e0c11
Thomas Hofmann <thomas@savoy-truffle.de>
2026-04-27 17:04:33 +0200

    refactoring: renamed local variable to enhance readability

--- verify-disksize.sh
+++ verify-disksize.sh
@@ -3,10 +3,12 @@ set -e
 
 DEV=${1?"DEVICE parameter missing"}
 
-read -p "WARNING: this will overwrite data. - Continue [y,N]?" -e shall_continue
-if [ "$shall_continue" != "y" ]
+read -p "WARNING: this will overwrite data on \"$DEV\". - Continue [y,N]?" -e SHALL_CONTINUE
+
+if [ "$SHALL_CONTINUE" != "y" ]
 then
     exit 0
 fi
 
 sudo f3probe --destructive --time-ops "$DEV"
+
